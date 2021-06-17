73766e48d2ab0a11b5775b80a36ab8c3936eab98
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-06-17 23:07:09 +0300

    Add clients section

--- client/src/pages/HowItWorks/index.jsx
+++ client/src/pages/HowItWorks/index.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import Header from '../../components/Header/Header';
-import StartScreen from '../../components/HowItWorks/StartScreen';
-import Services from '../../components/HowItWorks/Services';
-import Features from '../../components/HowItWorks/Features';
-import GetStarted from '../../components/HowItWorks/GetStarted';
-import Stats from '../../components/HowItWorks/Stats';
-import Questions from '../../components/HowItWorks/Questions';
-import Footer from '../../components/Footer/Footer';
+import React from 'react'
+import Header from '../../components/Header/Header'
+import StartScreen from '../../components/HowItWorks/StartScreen'
+import Services from '../../components/HowItWorks/Services'
+import Features from '../../components/HowItWorks/Features'
+import GetStarted from '../../components/HowItWorks/GetStarted'
+import Stats from '../../components/HowItWorks/Stats'
+import Questions from '../../components/HowItWorks/Questions'
+import Clients from '../../components/HowItWorks/Clients'
+import Footer from '../../components/Footer/Footer'
 
-
-const HowItWorks = (props) => {
+const HowItWorks = props => {
   return (
     <>
-      <Header/>
-      <StartScreen/>
-      <Services/>
-      <Features/>
-      <GetStarted/>
-      <Stats/>
-      <Questions/>
-      <Footer/>
+      <Header />
+      <StartScreen />
+      <Services />
+      <Features />
+      <GetStarted />
+      <Stats />
+      <Questions />
+      <Clients />
+      <Footer />
     </>
-  );
+  )
 }
 
-export default HowItWorks;
+export default HowItWorks
