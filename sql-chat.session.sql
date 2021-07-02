08ecd3cd35fb2a7af4f25735cb4ab32a0b058d50
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-07-02 07:39:12 +0300

    Delete commennts

--- sql-chat.session.sql
+++ sql-chat.session.sql
@@ -1,54 +1,4 @@
 CREATE SCHEMA chat;
-/* 
- DROP TABLE IF EXISTS chat.conversations;
- CREATE TABLE chat.conversations(
- id serial PRIMARY KEY,
- "createdAt" timestamp with time zone NOT NULL DEFAULT now(),
- "updatedAt" timestamp with time zone NOT NULL DEFAULT now()
- );
- -- 
- DROP TABLE IF EXISTS chat."participants";
- CREATE TABLE chat."participants"(
- id serial PRIMARY KEY,
- "userId" int REFERENCES "Users"
- );
- DROP TABLE IF EXISTS chat."conversationsToParticipants";
- CREATE TABLE chat."conversationsToParticipants"(
- id serial PRIMARY KEY,
- "conversationsId" int REFERENCES chat."conversations",
- "participantsId" int REFERENCES chat."participants"
- );
- -- 
- DROP TABLE IF EXISTS chat."blackList";
- CREATE TABLE chat."blackList"(
- id serial PRIMARY KEY,
- "userId" int REFERENCES "Users",
- "value" boolean
- );
- DROP TABLE IF EXISTS chat."conversationsToBlackList" ;
- CREATE TABLE chat."conversationsToBlackList"(
- id serial PRIMARY KEY,
- "conversationsId" int REFERENCES chat."conversations",
- "blackListId" int REFERENCES chat."blackList"
- );
- -- 
- DROP TABLE IF EXISTS chat."favoriteList";
- CREATE TABLE chat."favoriteList"(
- id serial PRIMARY KEY,
- "userId" int REFERENCES "Users",
- "value" boolean
- );
- DROP TABLE IF EXISTS chat."conversationsToFavoriteList"; 
- CREATE TABLE chat."conversationsToFavoriteList"(
- id serial PRIMARY KEY,
- "conversationsId" int REFERENCES chat."conversations",
- "favoriteListId" int REFERENCES chat."favoriteList"
- );
- 
- INSERT INTO chat."conversations"("createdAt") VALUES(now());
- -- 
- INSERT INTO chat.participants
- */
 DROP TABLE IF EXISTS chat.conversations;
 CREATE TABLE chat.conversations(
   id serial PRIMARY KEY,
@@ -77,21 +27,25 @@ DROP TABLE IF EXISTS chat."messages";
 CREATE TABLE chat."messages"(
   id serial PRIMARY KEY,
   "sender" int REFERENCES "Users",
-  "body" varchar(256) NOT NULL  CHECK("body" != '' AND "body" != ' '),
+  "body" varchar(256) NOT NULL CHECK(
+    "body" != ''
+    AND "body" != ' '
+  ),
   "conversation" int REFERENCES chat."conversations",
   "createdAt" timestamp with time zone NOT NULL DEFAULT now(),
   "updatedAt" timestamp with time zone NOT NULL DEFAULT now()
 );
- -- 
+-- 
 INSERT INTO chat.conversations("participants")
 VALUES (ARRAY [1,3]),
   (ARRAY [2,3])
 UPDATE chat.conversations
 SET "favoriteList" [2] = true;
 -- 
-INSERT INTO chat."messages"("sender","body" ,"conversation" ) VALUES(1,'hello', 1);
-INSERT INTO chat."messages"("sender","body" ,"conversation" ) VALUES(1,'hey', 1);
-
+INSERT INTO chat."messages"("sender", "body", "conversation")
+VALUES(1, 'hello', 1);
+INSERT INTO chat."messages"("sender", "body", "conversation")
+VALUES(1, 'hey', 1);
 -- 
 SELECT array_position("participants", 3)
 FROM chat.conversations;
@@ -103,7 +57,7 @@ VALUES (3, 'c1'),
 INSERT INTO chat."catalogsToConversations"
 VALUES (1, 1),
   (1, 3),
-(2, 1);
+  (2, 1);
 INSERT INTO chat.catalogs("userId", "catalogName")
 VALUES (2, 'b1');
 INSERT INTO chat."catalogsToConversations"
