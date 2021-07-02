9acdbe0b73e8f396c51d3b1a6a6e1f7494d7f382
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-07-02 07:38:49 +0300

    Add chat db prototype

---
+++ sql-chat.session.sql
@@ -0,0 +1,114 @@
+CREATE SCHEMA chat;
+/* 
+ DROP TABLE IF EXISTS chat.conversations;
+ CREATE TABLE chat.conversations(
+ id serial PRIMARY KEY,
+ "createdAt" timestamp with time zone NOT NULL DEFAULT now(),
+ "updatedAt" timestamp with time zone NOT NULL DEFAULT now()
+ );
+ -- 
+ DROP TABLE IF EXISTS chat."participants";
+ CREATE TABLE chat."participants"(
+ id serial PRIMARY KEY,
+ "userId" int REFERENCES "Users"
+ );
+ DROP TABLE IF EXISTS chat."conversationsToParticipants";
+ CREATE TABLE chat."conversationsToParticipants"(
+ id serial PRIMARY KEY,
+ "conversationsId" int REFERENCES chat."conversations",
+ "participantsId" int REFERENCES chat."participants"
+ );
+ -- 
+ DROP TABLE IF EXISTS chat."blackList";
+ CREATE TABLE chat."blackList"(
+ id serial PRIMARY KEY,
+ "userId" int REFERENCES "Users",
+ "value" boolean
+ );
+ DROP TABLE IF EXISTS chat."conversationsToBlackList" ;
+ CREATE TABLE chat."conversationsToBlackList"(
+ id serial PRIMARY KEY,
+ "conversationsId" int REFERENCES chat."conversations",
+ "blackListId" int REFERENCES chat."blackList"
+ );
+ -- 
+ DROP TABLE IF EXISTS chat."favoriteList";
+ CREATE TABLE chat."favoriteList"(
+ id serial PRIMARY KEY,
+ "userId" int REFERENCES "Users",
+ "value" boolean
+ );
+ DROP TABLE IF EXISTS chat."conversationsToFavoriteList"; 
+ CREATE TABLE chat."conversationsToFavoriteList"(
+ id serial PRIMARY KEY,
+ "conversationsId" int REFERENCES chat."conversations",
+ "favoriteListId" int REFERENCES chat."favoriteList"
+ );
+ 
+ INSERT INTO chat."conversations"("createdAt") VALUES(now());
+ -- 
+ INSERT INTO chat.participants
+ */
+DROP TABLE IF EXISTS chat.conversations;
+CREATE TABLE chat.conversations(
+  id serial PRIMARY KEY,
+  "participants" int ARRAY [2] NOT NULL,
+  "blackList" boolean ARRAY [2] NOT NULL DEFAULT ARRAY [false, false],
+  "favoriteList" boolean ARRAY [2] NOT NULL DEFAULT ARRAY [false, false],
+  "createdAt" timestamp with time zone NOT NULL DEFAULT now(),
+  "updatedAt" timestamp with time zone NOT NULL DEFAULT now()
+);
+/*  */
+DROP TABLE IF EXISTS chat.catalogs;
+CREATE TABLE chat.catalogs(
+  id serial PRIMARY KEY,
+  "userId" int REFERENCES "Users",
+  "catalogName" varchar(256) NOT NULL
+);
+/*  */
+DROP TABLE IF EXISTS chat."catalogsToConversations";
+CREATE TABLE chat."catalogsToConversations"(
+  "catalogId" int REFERENCES chat."catalogs",
+  "conversationId" int REFERENCES chat."conversations",
+  CONSTRAINT id PRIMARY KEY ("conversationId", "catalogId")
+);
+/*  */
+DROP TABLE IF EXISTS chat."messages";
+CREATE TABLE chat."messages"(
+  id serial PRIMARY KEY,
+  "sender" int REFERENCES "Users",
+  "body" varchar(256) NOT NULL  CHECK("body" != '' AND "body" != ' '),
+  "conversation" int REFERENCES chat."conversations",
+  "createdAt" timestamp with time zone NOT NULL DEFAULT now(),
+  "updatedAt" timestamp with time zone NOT NULL DEFAULT now()
+);
+ -- 
+INSERT INTO chat.conversations("participants")
+VALUES (ARRAY [1,3]),
+  (ARRAY [2,3])
+UPDATE chat.conversations
+SET "favoriteList" [2] = true;
+-- 
+INSERT INTO chat."messages"("sender","body" ,"conversation" ) VALUES(1,'hello', 1);
+INSERT INTO chat."messages"("sender","body" ,"conversation" ) VALUES(1,'hey', 1);
+
+-- 
+SELECT array_position("participants", 3)
+FROM chat.conversations;
+/*  */
+-- 
+INSERT INTO chat.catalogs("userId", "catalogName")
+VALUES (3, 'c1'),
+  (3, 'c2');
+INSERT INTO chat."catalogsToConversations"
+VALUES (1, 1),
+  (1, 3),
+(2, 1);
+INSERT INTO chat.catalogs("userId", "catalogName")
+VALUES (2, 'b1');
+INSERT INTO chat."catalogsToConversations"
+VALUES (3, 3);
+-- 
+SELECT *
+FROM chat.catalogs AS "c"
+  JOIN chat."catalogsToConversations" AS "ctc" ON "c".id = "ctc"."catalogId";
