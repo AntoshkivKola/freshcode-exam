85acd8a1821e1fca3ef5b870f2f8fd0e54e162d4
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-07-04 08:43:48 +0300

    Add chat controller for postgres chat

---
+++ server/src/server/controllers/chatControllerSQL.js
@@ -0,0 +1,94 @@
+const _ = require('lodash');
+const db = require('../models/index');
+const userQueries = require('./queries/userQueries');
+const controller = require('../../socketInit');
+const {Message,Conversation,Catalog} = require('../models/chatModels');
+
+module.exports.addMessage = async (req, res, next) => {
+  const participants = [req.tokenData.userId, req.body.recipient];
+  participants.sort(
+    (participant1, participant2) => participant1 - participant2);
+  try {
+    const newConversation = await Conversation.findOneOrCreate(participants)
+    console.log("newCon: ", newConversation)
+    
+    const message = new Message({
+      sender: req.tokenData.userId,
+      body: req.body.messageBody,
+      conversation: newConversation.id
+    });
+   
+    await message.save();
+    message.participants = participants;
+    const interlocutorId = participants.filter(
+      (participant) => participant !== req.tokenData.userId)[0];
+    const preview = {
+      _id: newConversation.id,
+      sender: req.tokenData.userId,
+      text: req.body.messageBody,
+      createAt: message.createdAt,
+      participants,
+      blackList: newConversation.blackList,
+      favoriteList: newConversation.favoriteList
+    };
+    const sender = await db.User.findByPk(req.tokenData.userId,{
+      attributes: ['firstName','lastName','id'],
+      raw: true,
+    })
+    controller.getChatController().emitNewMessage(interlocutorId, {
+      message: message,
+      preview: {
+        _id: newConversation.id,
+        sender: req.tokenData.userId,
+        text: req.body.messageBody,
+        createAt: message.createdAt,
+        participants,
+        blackList: newConversation.blackList,
+        favoriteList: newConversation.favoriteList,
+        interlocutor: {
+          id: req.tokenData.userId,
+          firstName: req.tokenData.firstName,
+          lastName: req.tokenData.lastName,
+          displayName: req.tokenData.displayName,
+          avatar: req.tokenData.avatar,
+          email: req.tokenData.email
+        }
+      }
+    });
+    controller.getNotificationController().emitNewMessage({
+      interlocutorId, 
+      sender, 
+      dialogId: message.conversation
+    })
+    res.send({
+      message,
+      preview: Object.assign(preview, { interlocutor: req.body.interlocutor })
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getChat = async (req, res, next) => {
+  const participants = [req.tokenData.userId, req.body.interlocutorId];
+  participants.sort(
+    (participant1, participant2) => participant1 - participant2);
+  try {
+    const messages = await Message.getMessages(participants);
+
+    const interlocutor = await userQueries.findUser(
+      { id: req.body.interlocutorId });
+    res.send({
+      messages,
+      interlocutor: {
+        firstName: interlocutor.firstName,
+        lastName: interlocutor.lastName,
+        displayName: interlocutor.displayName,
+        id: interlocutor.id,
+        avatar: interlocutor.avatar
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+};
