b4a96303232ae06c493c0fce114dfabfce059c53
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-07-08 21:34:18 +0300

    Add errorLoger

--- server/src/server/handlerError/handler.js
+++ server/src/server/handlerError/handler.js
@@ -4,10 +4,14 @@ const { errorLoger } = require('../utils/errorLoger');
 
 module.exports.tokenErrorHandler = (err, req, res, next) => {
   if (err instanceof TokenExpiredError) {
+    err.status = 419;
+    errorLoger(err);
     return res.status(419).send('Token expired');
   }
 
   if (err instanceof JsonWebTokenError) {
+    err.status = 401;
+    errorLoger(err);
     return res.status(401).send('Invalid token');
   }
 
@@ -19,6 +23,8 @@ module.exports.sequelizeErrorHandler = (err, req, res, next) => {
     const {
       errors: [{ message }],
     } = err;
+    err.status = 409;
+    errorLoger(err);
     return res.status(409).send({ message });
   }
   next(err);
@@ -27,11 +33,10 @@ module.exports.sequelizeErrorHandler = (err, req, res, next) => {
 module.exports.validationErrorHandler = () => {};
 
 module.exports.basicErrorHandler = (err, req, res, next) => {
-  /*   console.log('LOG ERROR=>>>>');
-  console.dir(err); */
-  errorLoger(err);
   if (!err.message || !err.status) {
+    err.status = 500;
     return res.status(500).send('Server Error');
   }
+  errorLoger(err);
   res.status(err.status).send(err.message);
 };
