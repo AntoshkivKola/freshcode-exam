bc79502686d95bbb71b58833a4594942ad246b1b
Atnoshkiv Mykola <ak.pkqtor@gmail.com>
2021-08-03 13:28:17 +0300

    Refactor form redox-form to formik

--- client/src/components/AgreeTermOfServiceInput/AgreeTermOfServiceInput.js
+++ client/src/components/AgreeTermOfServiceInput/AgreeTermOfServiceInput.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useField } from "formik";
 
-
-const AgreeTermOfServiceInput = ({label, id,input, type, classes, meta: {touched, error}}) => {
-    return (
-        <div>
-            <div className={classes.container}>
-                <input {...input} placeholder={label} id={id} type={type}/>
-                <label htmlFor={id}>By clicking this checkbox, you agree to our <a
-                    href="https://www.google.com" target={'_blank'}>Terms of Service.</a></label>
-            </div>
-            {touched && (error && <span className={classes.warning}>{error}</span>)}
-        </div>
-
-    )
+const AgreeTermOfServiceInput = (props) => {
+  const {
+    label,
+    id,
+    type,
+    classes,
+  
+  } = props;
+  const [field,  { touched, error }] = useField(props);
+  return (
+    <div>
+      <div className={classes.container}>
+        <input {...field} placeholder={label} id={id} type={type} />
+        <label htmlFor={id}>
+          By clicking this checkbox, you agree to our{" "}
+          <a href="https://www.google.com" target={"_blank"}>
+            Terms of Service.
+          </a>
+        </label>
+      </div>
+      {touched && error && <span className={classes.warning}>{error}</span>}
+    </div>
+  );
 };
 
 export default AgreeTermOfServiceInput;
--- client/src/components/RoleInput/RoleInput.js
+++ client/src/components/RoleInput/RoleInput.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import styles from './RoleInput.module.sass';
+import React from "react";
+import { useField } from "formik";
+import styles from "./RoleInput.module.sass";
 
+const RoleInput = (props) => {
+  const { id, strRole, infoRole, type } = props;
+  const [field, meta] = useField(props);
 
-const RoleInput = ({label, id, strRole, infoRole, input, type}) => {
-    return (
-        <label htmlFor={id}>
-            <div className={styles.roleContainer}>
-                <input {...input} type={type} id={id}/>
-                <div className={styles.infoRoleContainer}>
-                    <span className={styles.role}>{strRole}</span>
-                    <span className={styles.infoRole}>{infoRole}</span>
-                </div>
-            </div>
-        </label>
-    )
+  return (
+    <label htmlFor={id}>
+      <div className={styles.roleContainer}>
+        <input {...field} type={type} id={id} />
+        <div className={styles.infoRoleContainer}>
+          <span className={styles.role}>{strRole}</span>
+          <span className={styles.infoRole}>{infoRole}</span>
+        </div>
+      </div>
+    </label>
+  );
 };
 
 export default RoleInput;
