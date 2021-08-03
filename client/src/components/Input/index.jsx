079e0d0bf0ab363d0d1c3a3e6faaa4ac3d571ad7
Atnoshkiv Mykola <ak.pkqtor@gmail.com>
2021-08-03 15:55:21 +0300

    Refactor from redux-form to formik

--- client/src/components/Input/index.jsx
+++ client/src/components/Input/index.jsx
@@ -4,14 +4,24 @@ import cx from "classnames";
 import styles from "./Input.module.scss";
 
 const Input = (props) => {
-  const { name, placeholder, type } = props;
+  const {
+    name,
+    placeholder,
+    type,
+    classes = {
+      container: styles.inputContainer,
+      input: styles.input,
+      notValid: styles.notValid,
+      valid: styles.valid
+    },
+  } = props;
   const [field, meta] = useField(props);
-  const inputClassNames = cx(styles.input, {
-    [styles.notValid]: meta.touched && meta.error,
-    [styles.valid]: meta.touched && !meta.error,
+  const inputClassNames = cx(classes.input, {
+    [classes.notValid]: meta.touched && meta.error,
+    [classes.valid]: meta.touched && !meta.error,
   });
   return (
-    <label className={styles.inputContainer}>
+    <label className={classes.container}>
       <input
         type={type}
         {...field}
--- client/src/components/OfferForm/OfferForm.js
+++ client/src/components/OfferForm/OfferForm.js
@@ -1,93 +1,108 @@
-import React from 'react';
-import CONTANTS from '../../constants';
-import {connect} from 'react-redux';
-import {setOffer, clearAddOfferError} from '../../actions/actionCreator';
-import {withRouter} from 'react-router-dom';
-import styles from './OfferForm.module.sass';
-import {reduxForm, Field} from 'redux-form';
-import ImageUpload from '../InputComponents/ImageUpload/ImageUpload';
-import FormInput from '../FormInput/FormInput';
-import customValidator from "../../validators/validator";
+import React, {useState} from "react";
+import { connect } from "react-redux";
+import { Formik, Form } from "formik";
+import CONTANTS from "../../constants";
+import { setOffer, clearAddOfferError } from "../../actions/actionCreator";
 import Schems from "../../validators/validationSchems";
-import Error from '../../components/Error/Error';
-
+import ImageUpload from "../InputComponents/ImageUpload/ImageUpload";
+import Error from "../../components/Error/Error";
+import Input from "../Input";
+import styles from "./OfferForm.module.sass";
 
 let contestType;
 
 const OfferForm = (props) => {
-    const renderOfferInput = () => {
-        if (props.contestType === CONTANTS.LOGO_CONTEST) {
-            return (
-                <Field
-                    name='offerData'
-                    component={ImageUpload}
-                    classes={{
-                        uploadContainer: styles.imageUploadContainer,
-                        inputContainer: styles.uploadInputContainer,
-                        imgStyle: styles.imgStyle
-                    }}
-                />
-            )
-        } else {
-            return (
-                <Field
-                    name='offerData'
-                    classes={{
-                        container: styles.inputContainer,
-                        input: styles.input,
-                        warning: styles.fieldWarning,
-                        notValid: styles.notValid
-                    }}
-                    component={FormInput}
-                    type='text'
-                    label='your suggestion'
-                />
-            )
-        }
-    };
+    const [file, setFile] = useState(null)
+  const validationSchema =
+    contestType === CONTANTS.LOGO_CONTEST
+      ? Schems.LogoOfferSchema
+      : Schems.TextOfferSchema;
 
+  const renderOfferInput = () => {
+    if (props.contestType === CONTANTS.LOGO_CONTEST) {
+      return (
+        <ImageUpload
+          name="offerData"
+          component={ImageUpload}
+          classes={{
+            uploadContainer: styles.imageUploadContainer,
+            inputContainer: styles.uploadInputContainer,
+            imgStyle: styles.imgStyle,
+          }}
+          setFile={setFile}
+        />
+      );
+    } else {
+      return (
+        <Input
+          name="offerData"
+          classes={{
+            container: styles.inputContainer,
+            input: styles.input,
+            warning: styles.fieldWarning,
+            notValid: styles.notValid,
+          }}
+          type="text"
+          label="your suggestion"
+        />
+      );
+    }
+  };
 
-    const setOffer = (values) => {
-        props.clearOfferError();
-        const data = new FormData();
-        const {contestId, contestType, customerId, reset} = props;
-        data.append('contestId', contestId);
-        data.append('contestType', contestType);
-        data.append('offerData', values.offerData);
-        data.append('customerId', customerId);
-        props.setNewOffer(data);
-        reset();
-    };
+  const setOffer = (values, formikBag) => {
+    props.clearOfferError();
+    const data = new FormData();
+    const { contestId, contestType, customerId } = props;
+    console.log(contestId, contestType, customerId,file )
+    data.append("contestId", contestId);
+    data.append("contestType", contestType);
+    data.append("offerData",  contestType === CONTANTS.LOGO_CONTEST ? file : values.offerData);
+    data.append("customerId", customerId);
+    console.log(data.get('offerData'));
+    props.setNewOffer(data);
+    formikBag.resetForm();
+  };
 
-    contestType = props.contestType;
-    const {handleSubmit, valid, addOfferError, clearOfferError} = props;
-    return (
-        <div className={styles.offerContainer}>
-            {addOfferError &&
-            <Error data={addOfferError.data} status={addOfferError.status} clearError={clearOfferError}/>}
-            <form onSubmit={handleSubmit(setOffer)} className={styles.form}>
-                {renderOfferInput()}
-                {valid && <button type='submit' className={styles.btnOffer}>Send Offer</button>}
-            </form>
-        </div>
-    )
+  contestType = props.contestType;
+  const { addOfferError, clearOfferError } = props;
+  return (
+    <Formik
+      onSubmit={setOffer}
+      initialValues={{
+        offerData: "",
+      }}
+      validationSchema={validationSchema}
+    >
+      <div className={styles.offerContainer}>
+        {addOfferError && (
+          <Error
+            data={addOfferError.data}
+            status={addOfferError.status}
+            clearError={clearOfferError}
+          />
+        )}
+        <Form className={styles.form}>
+          {renderOfferInput()}
+
+          <button type="submit" className={styles.btnOffer}>
+            Send Offer
+          </button>
+        </Form>
+      </div>
+    </Formik>
+  );
 };
 
-
 const mapDispatchToProps = (dispatch) => {
-    return {
-        setNewOffer: (data) => dispatch(setOffer(data)),
-        clearOfferError: () => dispatch(clearAddOfferError())
-    }
+  return {
+    setNewOffer: (data) => dispatch(setOffer(data)),
+    clearOfferError: () => dispatch(clearAddOfferError()),
+  };
 };
 
 const mapStateToProps = (state) => {
-    const {addOfferError} = state.contestByIdStore;
-    return {addOfferError};
+  const { addOfferError } = state.contestByIdStore;
+  return { addOfferError };
 };
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({
-    form: 'offerForm',
-    validate: customValidator(contestType === CONTANTS.LOGO_CONTEST ? Schems.LogoOfferSchema : Schems.TextOfferSchema)
-})(OfferForm));
+export default connect(mapStateToProps, mapDispatchToProps)(OfferForm);
