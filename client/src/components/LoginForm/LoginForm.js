061d57d6152788626e1c79acd0169a8aed3e0cac
Atnoshkiv Mykola <ak.pkqtor@gmail.com>
2021-08-03 12:32:34 +0300

    Refactor from redux-form to formik

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,86 +1,77 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { authActionLogin, clearAuth } from '../../actions/actionCreator';
-import { Redirect, Link } from 'react-router-dom';
-import styles from './LoginForm.module.sass';
-import { Field, reduxForm } from 'redux-form';
-import FormInput from '../FormInput/FormInput';
-import customValidator from '../../validators/validator';
-import Schems from '../../validators/validationSchems';
-import Error from '../../components/Error/Error';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Formik, Form } from "formik";
+import Schems from "../../validators/validationSchems";
+import { authActionLogin, clearAuth } from "../../actions/actionCreator";
+import Error from "../../components/Error/Error";
+import Input from "../Input";
+import styles from "./LoginForm.module.sass";
+import commonStyles from '../../styles/common.module.scss';
 
-class LoginForm extends React.Component{
+const LoginForm = (props) => {
+  const { error, isFetching } = props.auth;
+  const { submitting, authClear } = props;
 
-  componentWillUnmount () {
-    this.props.authClear();
-  }
-
-  clicked = (values) => {
-    this.props.loginRequest(values);
+  const onSubmit = (values, formikBag) => {
+    props.loginRequest(values);
+    formikBag.resetForm();
   };
 
-  render () {
-    const {error, isFetching} = this.props.auth;
-    const {handleSubmit, submitting, authClear} = this.props;
-
-    const formInputClasses = {
-      container: styles.inputContainer,
-      input: styles.input,
-      warning: styles.fieldWarning,
-      notValid: styles.notValid,
-      valid: styles.valid,
-    };
-
-    return (
-      <div className={ styles.loginForm }>
-        { error && <Error data={ error.data } status={ error.status }
-                          clearError={ authClear }/> }
+  return (
+    <Formik
+      onSubmit={onSubmit}
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={Schems.LoginSchem}
+    >
+      <div className={styles.loginForm}>
+        {error && (
+          <Error
+            data={error.data}
+            status={error.status}
+            clearError={authClear}
+          />
+        )}
         <h2>LOGIN TO YOUR ACCOUNT</h2>
-        <form onSubmit={ handleSubmit(this.clicked) }>
-          <Field
-            name='email'
-            classes={ formInputClasses }
-            component={ FormInput }
-            type='text'
-            label='Email Address'
-          />
-          <Field
-            name='password'
-            classes={ formInputClasses }
-            component={ FormInput }
-            type='password'
-            label='password'
-          />
-          <button type='submit' disabled={ submitting }
-                  className={ styles.submitContainer }>
-            <span className={ styles.inscription }>{ isFetching
-              ? 'Submitting...'
-              : 'LOGIN' }</span>
+        <Form className={commonStyles.form}>
+          <Input name="email" type="email" placeholder="Email Address" />
+          <Input name="password" type="password" placeholder="Password" />
+
+          <button
+            type="submit"
+            disabled={submitting}
+            className={styles.submitContainer}
+          >
+            <span className={styles.inscription}>
+              {isFetching ? "Submitting..." : "LOGIN"}
+            </span>
           </button>
           <div className={styles.linkWrapper}>
-            <Link to='/changePassword'style={ {textDecoration: 'none'}  } className={ styles.forgotPasswordLink}>
+            <Link
+              to="/changePassword"
+              style={{ textDecoration: "none" }}
+              className={styles.forgotPasswordLink}
+            >
               <span>Forgot Password</span>
             </Link>
           </div>
-        </form>
+        </Form>
       </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  const {auth} = state;
-  return {auth};
+    </Formik>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => (
-  {
-    loginRequest: (data) => dispatch(authActionLogin(data)),
-    authClear: () => dispatch(clearAuth()),
-  }
-);
+const mapStateToProps = (state) => {
+  const { auth } = state;
+  return { auth };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({
-  form: 'login',
-  validate: customValidator(Schems.LoginSchem),
-})(LoginForm));
+const mapDispatchToProps = (dispatch) => ({
+  loginRequest: (data) => dispatch(authActionLogin(data)),
+  authClear: () => dispatch(clearAuth()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
