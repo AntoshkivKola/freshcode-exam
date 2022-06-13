3fdc1ca891087954c84edbe3d794526d9dde9f75
Mykola <ak.pkqtor@gmail.com>
2022-06-13 18:27:07 +0300

    Fix problem with redirect to /login when reload page

--- client/src/components/PrivateRoute/PrivateRoute.js
+++ client/src/components/PrivateRoute/PrivateRoute.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Spinner from '../Spinner/Spinner';
+import constants from "../../constants";
 
 const PrivateRoute = ({ roles, ...rest }) => {
   const { user, isFetching } = useSelector(state => state.auth)
+  const refreshToken = localStorage.getItem(constants.REFRESH_TOKEN)
 
   if (isFetching) {
     return <Spinner />
@@ -17,7 +19,11 @@ const PrivateRoute = ({ roles, ...rest }) => {
     return <Route {...rest} />
   }
 
-  return <Redirect to='/login' />
+  if (!refreshToken) {
+    return <Redirect to='/login' />
+  }
+
+  return null;
 }
 
 export default PrivateRoute
